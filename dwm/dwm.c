9c58f304dd320196e338dc910140d3aa163d98fb
Mateusz Szymiec <mattszymiec@gmail.com>
2022-06-21 22:27:23 +0200

    Add border and gap if only one client

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1354,7 +1354,7 @@ resizeclient(Client *c, int x, int y, int w, int h)
 		gapincr = gapoffset = 0;
 	} else {
 		/* Remove border and gap if layout is monocle or only one client */
-		if (c->mon->lt[c->mon->sellt]->arrange == monocle || n == 1) {
+		if (c->mon->lt[c->mon->sellt]->arrange == monocle) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
